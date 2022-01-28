e5a46186c07dc6a3d3112ca6ad5a0667943d56b4
railmj <railmj@ttu.ee>
2022-01-28 23:57:45 +0200

    first

---
+++ webpack-skeleton/README.md
@@ -0,0 +1,4 @@
+packages
+~~~sh
+npm install --save.dev html-webpack-plugin webpack webpack-cli webpack-dev-server
+~~~
---
+++ webpack-skeleton/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: "./src/index.html",
+                inject: "body",
+                minify: false
+            }
+        )
+    ]
+}
